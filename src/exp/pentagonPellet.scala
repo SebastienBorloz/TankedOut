214bb4dd686a964c36d6c81e4554b637b1aa903d
SebDuFond <sebborloz01@gmail.com>
2023-06-09 15:04:04 +0200

    balles fonctionnelles

--- src/exp/pentagonPellet.scala
+++ src/exp/pentagonPellet.scala
@@ -5,5 +5,5 @@ import com.badlogic.gdx.math.Vector2
 
 class pentagonPellet(positionIn: Vector2, valIn: Int = 25, healthIn: Int = 25, bodyDamageIn: Int = 25) extends Pellet(valIn, healthIn, bodyDamageIn, positionIn){
   val vectTria: Array[Vector2] = Array(new Vector2(0, 0), new Vector2(0, 25), new Vector2(23.75f, 32.5f), new Vector2(38.5f, 12.5f), new Vector2(23.75f, -7.5f))
-  val pentagonBox = new PhysicsPolygon("pentagonPellet", position, vectTria, 30, 1, 30, true)
+  val pentagonBox = new PhysicsPolygon("pentagonPellet", position, vectTria, 60, 0.1f, 1, true)
 }

ab4e414335dffc445d4b4d2f8f468fae746a5f47
SebDuFond <sebborloz01@gmail.com>
2023-06-09 15:21:54 +0200

    modif modele physique pellets

--- src/exp/pentagonPellet.scala
+++ src/exp/pentagonPellet.scala
@@ -5,5 +5,5 @@ import com.badlogic.gdx.math.Vector2
 
 class pentagonPellet(positionIn: Vector2, valIn: Int = 25, healthIn: Int = 25, bodyDamageIn: Int = 25) extends Pellet(valIn, healthIn, bodyDamageIn, positionIn){
   val vectTria: Array[Vector2] = Array(new Vector2(0, 0), new Vector2(0, 50), new Vector2(42.5f, 65), new Vector2(77, 25), new Vector2(42.5f, -15))
-  val pentagonBox = new PhysicsPolygon("pentagonPellet", position, vectTria, 30, 1, 30, true)
+  val pentagonBox = new PhysicsPolygon("pentagonPellet", position, vectTria, 60, 0.1f, 1, true)
 }

c8abe7a19bcb468f7c7230b7878d5c7edb59500d
jijiduc <jijiduc23@gmail.com>
2023-06-14 08:29:27 +0200

    déplacement bullshit

--- src/composants/Joueur.scala
+++ src/composants/Joueur.scala
@@ -1,28 +1,19 @@
 package composants
 
-import ch.hevs.gdx2d.components.physics.primitives.PhysicsBox
 import ch.hevs.gdx2d.components.physics.primitives.PhysicsCircle
-import ch.hevs.gdx2d.components.physics.utils.PhysicsConstants
 import ch.hevs.gdx2d.lib.GdxGraphics
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.Vector2
 import ch.hevs.gdx2d.lib.interfaces.DrawableObject
-import com.badlogic.gdx.{Gdx, Input}
-import exp.PelletFactory
+import exp.{Pellet, PelletFactory}
 
-import java.awt.MouseInfo.getPointerInfo
-import java.util
 import scala.collection.mutable.ArrayBuffer
 
-
-class Joueur(val bouboules: PelletFactory,val ray: Float, val inPosition: Vector2, val angle: Float) extends DrawableObject {
+class Joueur(val bouboules: PelletFactory, ray: Float, val inPosition: Vector2, val angle: Float) extends DrawableObject {
     val playerBox = new PhysicsCircle("playerCenter", inPosition, ray, angle)
     playerBox.setCollisionGroup(-1)
-    // Initialize canon
-    //this.canon = new Canon(this, playerBox.getBodyPosition, 50, 20)
-    private val stats: statSheet = new statSheet(1, 1, 1, 1 , 8, 1)
+    private val stats: statSheet = new statSheet(1, 1, 1, 1, 8, 1)
     var Boulettes: ArrayBuffer[Bullet] = new ArrayBuffer[Bullet]()
-    //protected var canon: Canon = null
     var moveRight = false
     var moveLeft = false
     var moveUp = false
@@ -30,15 +21,8 @@ class Joueur(val bouboules: PelletFactory,val ray: Float, val inPosition: Vector
     var shooting = false
     var shootingTemp = 0L
     var mouseAngle: Float = 0
+    var scalar: Int = 50
     var exp: Int = 0
-
-    def setSpeed(speed: Float): Unit = {
-        /*   speed - speed in kilometers per hour   */
-        var velocity = playerBox.getBodyLinearVelocity
-        velocity = velocity.nor
-        velocity = new Vector2(velocity.x * ((speed * 1000.0f) / 3600.0f), velocity.y * ((speed * 1000.0f) / 3600.0f))
-        playerBox.setBodyLinearVelocity(velocity)
-    }
 
     def getPos: Vector2 = playerBox.getBodyPosition
 
@@ -47,54 +31,95 @@ class Joueur(val bouboules: PelletFactory,val ray: Float, val inPosition: Vector
         g.drawFilledCircle(pos.x, pos.y, 30, Color.FIREBRICK)
     }
 
-    def getSpeedKMH: Float = {
-        val velocity = playerBox.getBodyLinearVelocity
-        val len = velocity.len
-        (len / 1000) * 3600
-    }
+    /** Fonction de gestion des déplacements du joueur et des tirs*/
+    def update(deltaTime: Float): Unit = {
 
-    def getLocalVelocity: Vector2 = {
-        playerBox.getBody.getLocalVector(playerBox.getBody.getLinearVelocityFromLocalPoint(new Vector2(0, 0)))
-    }
-
-    def update(deltaTime: Float): Unit = { // update revolving wheels
-        //creation de la direction
+        // Création de la direction
         var baseVector = new Vector2(0, 0)
-        if (moveUp) {
+        // Création et application d'un vecteur de force
+        var forceVector = baseVector.scl(scalar)
+        val position = playerBox.getBodyWorldCenter
+
+        /** déplacement vertical haut */
+        if (moveUp && !moveDown && !moveLeft && !moveRight) {
             baseVector.y += 1
+            forceVector = baseVector.scl(scalar)
+            //playerBox.setBodyLinearVelocity(forceVector)
+            playerBox.applyBodyForce(forceVector, position, true)
         }
-        if (moveDown) {
+        /** déplacement vertical bas */
+        if (moveDown && !moveUp && !moveLeft && !moveRight) {
             baseVector.y -= 1
+            forceVector = baseVector.scl(scalar)
+            //playerBox.setBodyLinearVelocity(forceVector)
+            playerBox.applyBodyForce(forceVector, position, true)
         }
-        if (moveLeft) {
+        /** déplacement horizontal gauche */
+        if (moveLeft && !moveDown && !moveUp && !moveRight) {
             baseVector.x -= 1
+            forceVector = baseVector.scl(scalar)
+            //playerBox.setBodyLinearVelocity(forceVector)
+            playerBox.applyBodyForce(forceVector, position, true)
         }
-        if (moveRight) {
+        /** déplacement horizontal droite */
+        if (moveRight && !moveDown && !moveUp && !moveLeft) {
             baseVector.x += 1
+            forceVector = baseVector.scl(scalar)
+            //playerBox.setBodyLinearVelocity(forceVector)
+            layerBox.applyBodyForce(forceVector, position, true)
+        }
+        if (moveRight && moveDown && !moveUp && !moveLeft) {
+            baseVector.x += 1
+            baseVector.y -= 1
+            forceVector = baseVector.scl(scalar)
+            //playerBox.setBodyLinearVelocity(forceVector)
+            playerBox.applyBodyForce(forceVector, position, true)
+        }
+        if (moveRight && !moveDown && moveUp && !moveLeft) {
+            baseVector.x += 1
+            baseVector.y += 1
+            forceVector = baseVector.scl(scalar)
+            //playerBox.setBodyLinearVelocity(forceVector)
+            playerBox.applyBodyForce(forceVector, position, true)
+        }
+        if (!moveRight && !moveDown && moveUp && moveLeft) {
+            baseVector.x -= 1
+            baseVector.y += 1
+            forceVector = baseVector.scl(scalar)
+            //playerBox.setBodyLinearVelocity(forceVector)
+            playerBox.applyBodyForce(forceVector, position, true)
+        }
+        if (!moveRight && moveDown && !moveUp && moveLeft) {
+            baseVector.x -= 1
+            baseVector.y -= 1
+            forceVector = baseVector.scl(scalar)
+            //playerBox.setBodyLinearVelocity(forceVector)
+            playerBox.applyBodyForce(forceVector, position, true)
         }
 
-        //ralenti si aucun bouton est enclenché
+        // Le joueur ralentit si aucune touche directionnelle n'est pressée
         if (!moveUp && !moveDown && !moveLeft && !moveRight) {
             baseVector = new Vector2(0, 0)
-            baseVector = playerBox.getBodyLinearVelocity().scl(-0.75f)
+            //playerBox.setBodyLinearVelocity(baseVector)
+            playerBox.applyBodyForce(forceVector, position, true)
         }
-        val forceVector = baseVector.scl(25) //multiplicateur vitesse
-
-        //application de la force
-        val position = playerBox.getBodyWorldCenter
-        playerBox.applyBodyForce(forceVector, position, true)
 
 
-        //limitation de vitesse
+        // Limitation de la vitesse maximum
         val longActu: Float = playerBox.getBodyLinearVelocity.len()
-        val vitesseLimite: Int = 15 * stats.movementSpeed
+        val vitesseLimite: Int = 10 * stats.movementSpeed
         if (longActu > vitesseLimite) {
             playerBox.setBodyLinearVelocity(playerBox.getBodyLinearVelocity.scl(vitesseLimite / longActu))
         }
-
-
-        //gestion des tirs
-        if(shooting == true && shootingTemp < System.currentTimeMillis() - 1000/stats.reload){
+        // Gestion du mouvement de recul suite à un tir
+        if (shooting && playerBox.getBodyLinearVelocity().isZero) {
+            println("Je tire CHEF !")
+        }else if(shooting) {
+            println("bouge !")
+            //playerBox.applyBodyForce(playerBox.getBodyLinearVelocity().scl(0.5f), playerBox.getBodyWorldCenter, true)
+        }
+        // Gestion des tirs
+        if(shooting && shootingTemp < System.currentTimeMillis() - 1000/stats.reload){
             shootingTemp = System.currentTimeMillis()
             val spawnPos: Vector2 = new Vector2(playerBox.getBodyPosition)
             spawnPos.x += 40 * math.sin(mouseAngle * math.Pi / 180).toFloat
@@ -104,9 +129,13 @@ class Joueur(val bouboules: PelletFactory,val ray: Float, val inPosition: Vector
                 spawnPos.y += 50 * math.cos(mouseAngle * math.Pi / 180).toFloat
             }
             Boulettes.addOne(new Bullet(this.Boulettes,this,10, 10, mouseAngle, spawnPos))
+            val backVector = baseVector.scl(-5)
+            val positionV = playerBox.getBodyWorldCenter
+            playerBox.applyBodyForce(backVector, positionV, true)
+
         }
     }
-
+    /** Fonction qui aligne le joueur en direction du curseur de la souris */
     def getAngle(v1: Vector2, v2: Vector2): Double = {
         val vInt1: Vector2 = if (v1.x > v2.x) {
             new Vector2(v2.x - v1.x, v2.y - v1.y)
@@ -118,7 +147,7 @@ class Joueur(val bouboules: PelletFactory,val ray: Float, val inPosition: Vector
         val dessous: Double = math.sqrt(math.pow(vInt1.x, 2) + math.pow(vInt1.y, 2)) * math.sqrt(math.pow(vInt2.x, 2) + math.pow(vInt2.y, 2))
         if (v1.y < 0) {
             dessus = dessus * -1
-        } //reparation au cas ou la souris est en dessous de la physicBox
+        } //Réparation au cas ou la souris est en dessous de la physicBox
         if (v1.x > v2.x) {
             math.acos(dessus / dessous) * 180.0 / math.Pi
         } else {
